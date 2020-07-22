199f3de0b0a31adab7c0a07c988110157e061a68
ggstoltz <gunnar.b.stoltz@gmail.com>
2020-07-22 13:59:21 -0500

    updates

--- DEV/script.js
+++ DEV/script.js
@@ -1,13 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Dom
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const randomFunc = {
+  upper: getRandomUpper,
+  lower: getRandomLower,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
 
-  passwordText.value = password;
+// Write password to the #password input http://www.net-comber.com/charset.html
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*-_.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 // Add event listener to generate button
